5420f42a46bec1b10faeaa1fa4eae59eae25019d
Chad Palmer <chad.palmer@yale.edu>
2020-08-03 14:26:07 +0000

    Automatically backed up by Learn

---
+++ example.rb
@@ -0,0 +1,6 @@
+def examplefunction(arg1 arg2)
+  puts arg1
+  puts arg2
+end
+
+examplefunction("hello" "hi")
---
+++ return.rb
@@ -0,0 +1,5 @@
+def returning(arg)
+  puts "Hello #{arg}, how are you"
+end
+
+puts returning(nil)
